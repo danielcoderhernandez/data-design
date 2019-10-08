a1a81b371c569d2b3284b6d59dfd897fad63fac9
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-08 10:28:16 -0600

    first code, basic html

---
+++ epic/persona.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<div lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>The One Who Knocks</title>
+	<script src="./script.js"></script>
+</head>
+<p>
+
+
+  <div class="wrapper">
+  <h1> Walter White</h1>
+	  <h2>Business Owner</h2>
+	  <p> Runs a eighty-millon dollar business </p>
+	  <p> Has a former student as a business partner </p>
+	  <p> Has a mobile business platform </p>
+	  <p> Prefers to be anonymous in his business dealings </p>
+  <p> Works long days and nights to keep business afloat </p>
+
+	  <h2> Family Man </h2>
+	  <p> Wife is Skyler White </p>
+	  <p> Son is Walter White, Jr. named after himself</p>
+	  <p> Daughter is Holly White named after Holly Holms</p>
+	  <p> He frequently picksup pizza for his family since he is always cooking for his customers </p>
+	<div class="center"><img src="./PC-494"></div>
+</div>
+</body>
