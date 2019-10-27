0fb72f9e363dfd9f71759a217c3557b0e15f287d
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-27 01:47:26 -0600

    prior update that had yet to be pushed

---
+++ epic/data-design-index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Index</title>
+	<link rel="stylesheet" href="./style.css">
+</head>
+<body>
+
+<div class="wrapper">
+	<h2>Knock Knock, Knock Knock</h2>
+
+
+</div>
+<h2><a href="./persona.php">Persona</a> </h2>
+<h2><a href="./usecase-interactionflow.php">Use Case - Interaction Flow</a> </h2>
+<h2><a href="./user-story.php">User Story</a> </h2>
+
+<div class="center">
+	<img src="./conceptual-model.PNG">
+	<img src="./diagram-erd-phase2.PNG">
+</div>
+
+
+<h2> <a href="./"</h2>
+</body>
