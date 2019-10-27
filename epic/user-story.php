93093992d4e299a8f30e35efa75635b401f084fe
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-27 01:47:57 -0600

    prior update that had yet to be pushed

--- epic/user-story.php
+++ epic/user-story.php
@@ -8,14 +8,12 @@
 <body>
 
 <div class="wrapper">
-<p>
+
 	<h2>"As a frequent buyer of pizza I want to order pizza without having to disclose my location" </h2>
-</p>
 	<div class="center">
-	<img src="./images.jpg" class="walterid"/>
+	<img src="./images.jpg" class="walterid">
 	</div>
 
 </div>
-<h2><a href="./persona.php">Persona</a> </h2>
-<h2><a href="./usecase-interactionflow.php">Use Case - Interaction Flow</a> </h2>
+	<h2><a href="./data-design-index.php">INDEX</a> </h2>
 </body>
