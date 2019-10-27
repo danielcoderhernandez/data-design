6592588eca4c3035a123c348224357f13f7e2388
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-27 01:49:08 -0600

    updated ddl due to feedback of erd

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,6 +1,5 @@
-drop table if exists cart;
-drop table if exists menu;
 drop table if exists `order`;
+drop table if exists menu;
 drop table if exists profile;
 
 create table profile(
@@ -15,26 +14,23 @@ create table profile(
 	primary key(profileId)
 );
 
-create table `order`(
-	orderId binary(16) not null,
-	orderHistory varchar(128) null,
-	orderProfileId binary (16),
-	orderType char(97) not null,
-	primary key(orderId)
-);
-
 create table menu(
-	menuOrderId binary(16) not null,
+	menuId binary(16) not null,
 	menuFoodTypes varchar(16) not null,
-	menuFulfillmentType varchar(16) not null,
-	menuQuantity varchar (32) not null,
 	menuSize char(16) not null,
 	menuToppings varchar(256) not null
 );
 
-create table cart(
-	cartOrderId binary(16) not null,
-	cartPayment varchar(24) not null,
-	cartProfileId binary(16) not null,
-	cartReview varchar (32) not null
+create table `order`(
+	orderMenuId          BINARY(16)  not null,
+	orderProfileId       BINARY(16),
+	orderFulfillmentTime CHAR(4)     not null,
+	orderHistory         VARCHAR(128),
+	orderPaymentType     CHAR(16)    not null,
+	orderQuantity        VARCHAR(16) not null,
+	orderType            VARCHAR(64) not null,
+	primary key(orderMenuId)
 );
+
+
+
