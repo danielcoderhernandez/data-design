1c9071bff0be08a09a77fea920a5566e9cb86360
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-27 02:02:52 -0600

    updated ddl to run

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -2,26 +2,26 @@ drop table if exists `order`;
 drop table if exists menu;
 drop table if exists profile;
 
-create table profile(
-	profileId binary(16) not null,
-	profileActivationToken char(32),
-	profileEmail varchar(128) not null,
-	profileName char(97) not null,
-	profilePassword varchar (32) not null,
-	profilePhone char (10) not null,
-	unique(profileEmail),
-	index(profileEmail),
-	primary key(profileId)
+create table profile (
+	profileId              BINARY(16)   not null,
+	profileActivationToken CHAR(32),
+	profileEmail           VARCHAR(128) not null,
+	profileName            CHAR(97)     not null,
+	profilePassword        VARCHAR(32)  not null,
+	profilePhone           CHAR(10)     not null,
+	unique (profileEmail),
+	index (profileEmail),
+	primary key (profileId)
 );
 
-create table menu(
-	menuId binary(16) not null,
-	menuFoodTypes varchar(16) not null,
-	menuSize char(16) not null,
-	menuToppings varchar(256) not null
+create table menu (
+	menuId        BINARY(16)   not null,
+	menuFoodTypes VARCHAR(16)  not null,
+	menuSize      CHAR(16)     not null,
+	menuToppings  VARCHAR(256) not null
 );
 
-create table `order`(
+create table `order` (
 	orderMenuId          BINARY(16)  not null,
 	orderProfileId       BINARY(16),
 	orderFulfillmentTime CHAR(4)     not null,
@@ -29,7 +29,7 @@ create table `order`(
 	orderPaymentType     CHAR(16)    not null,
 	orderQuantity        VARCHAR(16) not null,
 	orderType            VARCHAR(64) not null,
-	primary key(orderMenuId)
+	primary key (orderMenuId)
 );
 
 
